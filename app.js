5f6db9784bc633122894948d54ab3ba1eb1266b3
dobblexg <kennybajomo@gmail.com>
2020-12-27 03:04:39 +0100

    added {useCreateIndex: true} to the .connect method

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const db = require("./config/database");
 
 const app = express();
 
-mongoose.connect(db.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(db.dbURI, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true})
 .then((result) => {
   const port = process.env.PORT || 5000;
   app.listen(port, () => {
